7165b6bb5163a38f8918b3f30f8cb969da6d53ce
enter1996 <enter13794302@gmail.com>
2019-05-08 17:12:14 +0900

    commit

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -23,9 +23,9 @@ public class App
     	Greeter korea = (Greeter) ctx.getBean("korea");
     	// Greeter 클래스로부터 생성된 객체를 참조하는 변수,
     	// korea는 Greeter클래스에 정의된 메소드를 사용할 수 있다고 생각함
-        System.out.println( "Greeting!" + korea.sayHello() );
+        System.out.println( "Greeting!!!" + korea.sayHello() );
         
         Greeter2 eng = (Greeter2) ctx.getBean("english");
-        System.out.println( "Greeting!" + eng.sayHello() );
+        System.out.println( "Greeting!!!" + eng.sayHello() );
     }
 }
